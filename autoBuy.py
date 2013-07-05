067b0c1e0718ddd141f22bc431818d7c97bfc002
choigunho <choiguenho@gmail.com>
2013-07-05 16:10:05 +0900

    - sending SMS

--- autoBuy.py
+++ autoBuy.py
@@ -1,22 +1,21 @@
 #-*- coding: utf-8 -*-
 
 import urllib
-import re
+import urllib2, httplib2
+import json
+import re, time
 from BeautifulSoup import *
 from twilio.rest import TwilioRestClient
 
 url = "http://cafe985.daum.net/_c21_/bbs_list?grpid=tdBX&fldid=79XF" 
-keywords = [u"미즈노"]
+keywords = [u"롤링스"]
 
-html = urllib.urlopen(url)
-soup = BeautifulSoup(html)
-
-
+# Download the twilio-python library from http://twilio.com/docs/libraries
 def sendSMS(msg):
-	# Download the twilio-python library from http://twilio.com/docs/libraries
+
 	# Find these values at https://twilio.com/user/account
-	account_sid = "xxxxx"
-	auth_token = "xxxxx"
+	account_sid = "xxx"
+	auth_token = "yyy"
 	client = TwilioRestClient(account_sid, auth_token)
 	 
 	message = client.sms.messages.create(to="+821085611186", from_="+14344736169", body=msg)
@@ -29,30 +28,76 @@ def extractAttribute(node, classname):
 
 	if classname == 'subject':
 		if node.find('a').string == None:
+			# link = node.find('a').get('href')
 			return target.find('a').findNext().nextSibling.string
 		return target.find('a').string
 
 	return target.string
 
-def findItem(node):
-	
-	
+def extractUrl(node):
+	target = node.find('td', {'class':'subject'})
+
+	return target.find('a').get('href')
+
+def is_duplicated(item, url):
+	if item not in wishItems:
+		wishItems.append(item)
+
+		fullurl = "http://cafe985.daum.net" + url
+		# print fullurl
+
+		link = shorternUrl(fullurl)
+		# print link
+
+		result_dic = json.loads(link)
+		# print result_dic['id']
+
+		sendSMS(num + ',' + subject + ',' + result_dic['id'])
+
+def shorternUrl(url):
+    API_KEY = "AIzaSyCvhcU63u5OTnUsdYaCFtDkcutNm6lIEpw"
+    apiUrl = 'https://www.googleapis.com/urlshortener/v1/url'
+    longUrl = url
+    headers = {"Content-type": "application/json"}
+
+    data = {"longUrl": longUrl}
+    h = httplib2.Http('.cache')
+    
+    try:
+        headers, response = h.request(apiUrl, "POST", json.dumps(data), headers)
+        return response
+
+    except Exception, e:
+        return "unexpected error %s" % e
+
 
 
 if __name__ == '__main__':
 
-	table = soup('table', {'class':'bbsList'})
-	rows = table[0].find('tbody').findAll('tr')
+	wishItems = []
 
-	for row in rows:
-		if extractAttribute(row, 'num') != None:
-			
-			subject = extractAttribute(row, 'subject')
+	while True:
+		html = urllib.urlopen(url)
+		soup = BeautifulSoup(html)
 
-			for word in keywords:
-				if subject.find(word) == 1:
-					print extractAttribute(row, 'num'), extractAttribute(row, 'subject'), extractAttribute(row, 'nick'), extractAttribute(row, 'date')
-					#sendSMS(subject)
-			
+		table = soup('table', {'class':'bbsList'})
+		rows = table[0].find('tbody').findAll('tr')
 
-			# print extractAttribute(row, 'num'), extractAttribute(row, 'subject'), extractAttribute(row, 'nick'), extractAttribute(row, 'date')
+		for row in rows:
+			if extractAttribute(row, 'num') != None:
+				
+				num = extractAttribute(row, 'num')
+				subject = extractAttribute(row, 'subject')
+				nick = extractAttribute(row, 'nick')
+				date = extractAttribute(row, 'date')
+				url = extractUrl(row)
+
+				# print url
+
+				for word in keywords:
+					if subject.find(word) == 1:
+						print num, subject, nick, date
+						is_duplicated(num, url)
+
+		# print wishItems
+		time.sleep(60)
